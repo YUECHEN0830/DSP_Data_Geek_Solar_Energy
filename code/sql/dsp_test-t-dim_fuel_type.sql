a4ab95785b1fbdab4b45b5d377ebd96988bd5545
rato-li <rato_li@yahoo.com.au>
2020-10-20 12:30:32 +1100

    fixed SQL script to specify DEFAULT CURRENT_TIMESTAMP

--- code/sql/dsp_test-t-dim_fuel_type.sql
+++ code/sql/dsp_test-t-dim_fuel_type.sql
@@ -2,13 +2,14 @@ use dsp_test
 ;
 CREATE TABLE dim_fuel_type (
 	dim_fuel_type_key int(11) NOT NULL AUTO_INCREMENT
-	,dim_created_date datetime
-	,dim_updated_date datetime
+	,dim_created_date datetime DEFAULT CURRENT_TIMESTAMP
+	,dim_updated_date datetime DEFAULT CURRENT_TIMESTAMP
 	,fuel_group varchar(50)
 	,fuel_type varchar(50)
 	,CONSTRAINT dim_fuel_type_pk PRIMARY KEY (dim_fuel_type_key)
 )
 ;
+/*
 ALTER TABLE dim_fuel_type
   MODIFY COLUMN `dim_created_date` DATETIME DEFAULT CURRENT_TIMESTAMP
 ;
@@ -16,14 +17,6 @@ ALTER TABLE dim_fuel_type
    MODIFY COLUMN dim_updated_date DATETIME DEFAULT CURRENT_TIMESTAMP
 ;
 TRUNCATE TABLE dim_fuel_type 
-;
-/*
- * 
-CREATE TABLE fct_fuel_type (
-)
-;
-TRUNCATE TABLE fct_fuel_type 
-
 */
 ;
 /*
