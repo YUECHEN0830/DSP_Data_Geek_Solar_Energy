3d51df843f58980f97ccaa5f106e1133f1cbe05e
rato-li <rato_li@yahoo.com.au>
2020-10-23 01:46:33 +1100

    new R script load_fct_daily_solar_exposure

--- code/src/load_fct_daily_solar_exposure.R
+++ code/src/load_fct_daily_solar_exposure.R
@@ -13,42 +13,11 @@ conn_staging <- dbConnect(RMySQL::MySQL(),
                           host = 'dsp-01.cnk9sarev6lg.us-east-2.rds.amazonaws.com')
 
 ## Get staging records
-stg_rows <- dbSendQuery(conn_staging, "select
-	right(concat('0000',Site), 6) as station_number
-	,Name as station_name
-	,Lat as latitude
-	,Lon as longitude
-	,`Start Year` as start_year
-	,case `Start Month`
-		when 'Jan' then 1
-		when 'Feb' then 2
-		when 'Mar' then 3
-		when 'Apr' then 4
-		when 'May' then 5
-		when 'Jun' then 6
-		when 'Jul' then 7
-		when 'Aug' then 8
-		when 'Sep' then 9
-		when 'Oct' then 10
-		when 'Nov' then 11
-		when 'Dec' then 12
-	end as start_month
-	,`End Year` as end_year
-	,case `End Month`
-		when 'Jan' then 1
-		when 'Feb' then 2
-		when 'Mar' then 3
-		when 'Apr' then 4
-		when 'May' then 5
-		when 'Jun' then 6
-		when 'Jul' then 7
-		when 'Aug' then 8
-		when 'Sep' then 9
-		when 'Oct' then 10
-		when 'Nov' then 11
-		when 'Dec' then 12
-	end as end_month
-from stg_Weather_Stations
+stg_rows <- dbSendQuery(conn_staging, "SELECT
+	`Bureau of Meteorology station number` as station_number
+	,CAST(CONCAT(Year,LPAD(Month,2,'00'),LPAD(Day,2,'00')) as SIGNED) as dim_date_key
+	,CAST(`Daily global solar exposure (MJ/m*m)` AS DECIMAL(8,4)) as daily_exposure
+from db_dsp01.stg_IDCJAC0016_072150_1800_Data
 ;")
 ## transform into dataframe
 df_staging <- fetch(stg_rows)
@@ -66,22 +35,24 @@ conn_dwh <- dbConnect(RMySQL::MySQL(),
                       dbname = 'dsp_test',
                       host = 'tutorial-db-instance.ce9zfotawf0r.us-east-2.rds.amazonaws.com')
 
-curr_dim <- dbReadTable(conn_dwh,"dim_weather_station")
+dim_weather_station <- dbReadTable(conn_dwh,"dim_weather_station")
+fct_daily_solar_exposure <- dbReadTable(conn_dwh,"fct_daily_solar_exposure")
 
-# Wrangling of dataframe into the right form to store in SQL
 df_stg <- df_staging %>%
   # select(-value, -fin_year) %>%
-  distinct() %>%
-  arrange(station_number)
+  # distinct() %>%
+  arrange(station_number, dim_date_key)
 df_load <- df_stg %>%
-  left_join(curr_dim, by='station_number') %>%
-  filter(is.na(dim_weather_station_key)==TRUE)
+  left_join(dim_weather_station, by='station_number') %>%
+  left_join(fct_daily_solar_exposure, by=c('dim_weather_station_key','dim_date_key'))
 df_load <- df_load %>%
-  select(colnames(df_load[,1:8]))
-colnames(df_load) <- sub(".x", "", colnames(df_load))
-
+  filter(is.na(fct_daily_solar_exposure_key)==TRUE) %>%
+  select(c('dim_weather_station_key', 'dim_date_key', 'daily_exposure.x'))
+df_load <- df_load %>% rename("daily_exposure.x" = "daily_exposure")
 
 # Save Records into DIM table
 dbExecute(conn_dwh,"start transaction;")
-dbWriteTable(conn_dwh,"dim_weather_station",df_load, append=TRUE, row.names=FALSE)
+dbWriteTable(conn_dwh,"fct_daily_solar_exposure",df_load, append=TRUE, row.names=FALSE)
+#dbExecute(conn_dwh,"insert into myRealTable(a,b,c) select a,b,c from myTempTable")
+#dbExecute(conn_dwh,"drop table if exists myTempTable")
 dbExecute(conn_dwh,"commit;")
