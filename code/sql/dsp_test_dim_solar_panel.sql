a1a269028bb651f2fcb53fd53520dc77867c86dd
Marco-Sulser <69408205+Marco-Sulser@users.noreply.github.com>
2020-10-31 10:07:10 +1100

    Corrected Syntax

--- code/sql/dsp_test_dim_solar_panel.sql
+++ code/sql/dsp_test_dim_solar_panel.sql
@@ -1,6 +1,6 @@
 SELECT
-t1.Model,
-t1.Wattage PMAX,
-t1.Module Efficiency, 
-t1.Temperature Coefficient
+t1.`Model`,
+t1.`Wattage PMAX`,
+t1.`Module Efficiency`, 
+t1.`Temperature Coefficient`
 FROM dim_solar_panel t1; 
