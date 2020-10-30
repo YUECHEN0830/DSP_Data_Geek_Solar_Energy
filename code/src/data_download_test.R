d7ddec2fe42ccbc279473c49e8b81306d4505246
kevin <kevingyk@163.com>
2020-10-31 00:59:04 +1100

    add .html file to .gitignore; add a new demo to download data from BOM

--- code/src/data_download_test.R
+++ code/src/data_download_test.R
@@ -3,4 +3,6 @@ source(here("code/src", "bom_solar_data_download.R"))
 
 print_test(2)
 
-bom_solar_data_download(station_num=023115)
+bom_solar_data_download(station_num = 023115)
+bom_solar_data_download(data_type = 36, station_num = 3003, dest_folder_path = "data/raw_datasets/BOM_temp")
+
